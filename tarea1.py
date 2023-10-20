6339e20ae86a1c8ad04045fa1bdc198cdbd08d44
Micaela25 <77306813+Micaela25@users.noreply.github.com>
2023-10-20 20:43:35 -0300

    Add files via upload

---
+++ tarea1.py
@@ -0,0 +1,20 @@
+empleados_ventas = ["EM0001", "EM0002", "EM0003", "EM0005", "EM0006", "EM0010"]
+print(empleados_ventas)
+empleados_ventas.append("EM0004")
+print(empleados_ventas)
+empleados_ventas = ["EM0001", "EM0002", "EM0003", "EM0005", "EM0006", "EM0010"]
+print(empleados_ventas)
+empleados_ventas.remove("EM0006")
+print(empleados_ventas)
+comisiones = [1000, 1100, 1000, 1500, 1200, 1300]
+print(comisiones)
+Comisiones =[1000,1100,1000,1500,1200,1300]
+comision_minima = min(comisiones)
+comision_maxima = max(comisiones)
+
+print("Comisión Mínima:", comision_minima)
+print("Comisión Máxima:", comision_maxima)
+total_comisiones = sum(comisiones)
+
+print("Importe Total de Comisiones a Pagar:", total_comisiones)
+
